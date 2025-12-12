458fbd30395ffca98d5fe282ad1a0134a5dd2a76
ram <pvsram60@gmail.com>
2025-12-12 11:41:08 +0530

    Add business-profile.html to Vite build config

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,8 @@ export default defineConfig({
         rollupOptions: {
             input: {
                 main: resolve(__dirname, 'index.html'),
-                login: resolve(__dirname, 'login.html')
+                login: resolve(__dirname, 'login.html'),
+                profile: resolve(__dirname, 'business-profile.html')
             }
         }
     }
